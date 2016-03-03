6ff7fb2354557a9c349b4242d7b778ae5f58b67a
Saul Bertuccio <bsaul@inwind.it>
2016-03-03 07:56:45 +0100

    Riscrittura codice per la generazione statistiche

--- www/html/models/sensorstats.php
+++ www/html/models/sensorstats.php
@@ -7,36 +7,56 @@ class SensorStatsModel {
 		UMIDITA = 'umidita'
 	;
 
+	private static
+		$STATUS_OK = 0b1111,
+		$PHYQT = 0b0001,
+		$SID = 0b0010,
+		$START_DATE = 0b0100,
+		$END_DATE = 0b1000
+	;
+
+
 	private
 		$physicalQt = null,
 		$sensorId = null,
 		$start_date = null,
-		$end_date = null
+		$end_date = null,
+
+		$status = 0
 	;
 
 	public function __construct() { }
 
 	public function setPhysicalType($type) {
 		$this->physicalQt = $type;
+
+		$this->status |= self::$PHYQT;
+		d($this->status |= self::$PHYQT);
 		return $this;
 	}
 
 	public function setSensorId($sid) {
 		$this->sensorId = $sid;
+		$this->status |= self::SID;
 		return $this;
 	}
 
 	public function setStartDate($sd) {
 		$this->start_date = $sd;
+		$this->status |= self::$START_DATE;
 		return $this;
 	}
 
 	public function setEndDate($ed) {
 		$this->end_date = $ed;
+		$this->status |= self::$END_DATE;
 		return $this;
 	}
 
 	public function getData() {
+d($this->status);
+		if ($this->status !== self::$STATUS_OK)
+			return null;
 
 		$query = "SELECT EXTRACT(epoch FROM data_ora) * 1000,"
 				. "({$this->physicalQt})::numeric(5,2) FROM "
--- www/html/views/sensorstats.php
+++ www/html/views/sensorstats.php
@@ -1,24 +1,18 @@
 <?php
 
-class AndamentoView {
+class SensorStatsView {
 
 	private $model;
 	private $controller;
 
-	private $tplh;
-
 	public function __construct($controller, $model) {
+
 		$this->controller = $controller;
 		$this->model = $model;
-
-		$this->tplh = Template::get();
-
 	}
 
 	public function render() {
 
-		return $this->controller->isInitialized()
-			? json_encode($this->model->getData(), JSON_NUMERIC_CHECK)
-			: json_encode([]);
+		return json_encode($this->model->getData(), JSON_NUMERIC_CHECK);
 	}
 }

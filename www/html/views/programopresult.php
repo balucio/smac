5a7f5c03bbec4f36a9bd30e13213c45a5aedcb20
Saul Bertuccio <bsaul@inwind.it>
2016-03-13 22:44:59 +0100

    Aggiunta vista per gestire le risposte in caso di modifica o creazione di un programma

---
+++ www/html/views/programopresult.php
@@ -0,0 +1,22 @@
+<?php
+
+class ProgramOpResultView extends BaseView {
+
+	public function render() {
+
+		$status = $this->model->getStatus();
+
+		switch ($status) {
+			case Db::STATUS_OK : $msgid = ''; break;
+			case Db::STATUS_DUPLICATE : $msgid = 'message-err-duplicate'; break;
+			case Db::STATUS_ERR : $msgid = 'message-err-db'; break;
+			default: $msgid = 'message-err-data';
+		}
+
+		return json_encode((object)[
+			'status' => $status === Db::STATUS_OK,
+			'code' => $status,
+			'msgid' => $msgid
+		]);
+	}
+}

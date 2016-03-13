ec80bd8800e09437725d15b36160ddaa13857b0b
Saul Bertuccio <bsaul@inwind.it>
2016-03-13 22:31:28 +0100

    Aggiunta la funzione per poter modificare o creare un programma. Questa ha richiesto l'introduzione nel controller delle nuove variabili provenienti da POST (nome, descrizione, temperature, sensore) che definiscono un programma La funzione initFromRequest esegue adesso solo alcune validazioni base dei dati ricevuti. La validazione più approfondita è delegata alle varie funzioni del controller

--- www/html/controllers/programdata.php
+++ www/html/controllers/programdata.php
@@ -2,8 +2,15 @@
 
 class ProgramDataController extends BaseController {
 
+	const MIN_TEMP = 3.0;
+	const MAX_TEMP = 30.0;
+
 	private
 		$pid = null,
+		$name = null,
+		$descr = null,
+		$sensor = null,
+		$temps = [],
 		$day = null
 	;
 
@@ -13,11 +20,56 @@ class ProgramDataController extends BaseController {
 		$this->setDefaultAction('setProgramId');
 	}
 
-	public function setProgramId() { $this->model->setPid($this->pid, $this->day); }
+	public function setProgramId() {
+
+		if ($this->pid !== null && !Validate::IsInteger($this->pid))
+			$this->pid = null;
+
+		$this->model->setPid($this->pid, $this->day);
+	}
 
 	public function getProgramData() { $this->setProgramId();}
 
+	public function createOrUpdate() {
+
+		$pid = Request::Attr('program', null);
+
+			// Verifico che il pid originario sia comunque corretto
+		if ($pid != $this->pid)
+			return;
+
+			// Verifico sensore
+		if (!Validate::IsInteger($this->sensor))
+			return;
+
+			// Verifico presenza di
+		foreach (['name', 'descr', 'temps'] as $k)
+			if (empty($this->$k))
+				return;
+
+
+		foreach ($this->temps as &$t)
+			if (!Validate::IsFloatInRange($t, self::MIN_TEMP, self::MAX_TEMP ))
+				return;
+			else
+				$t = (float)$t;
+
+		sort( $this->temps, SORT_NUMERIC );
+		$this->temps = array_unique( $this->temps, SORT_NUMERIC );
+
+		$this->model->updateProgram(
+			$this->pid,
+			$this->name,
+			$this->descr,
+			$this->temps,
+			$this->sensor
+		);
+	}
+
 	public function saveDefault() {
+
+		if ($this->pid !== null && !Validate::IsInteger($this->pid))
+			$this->pid = null;
 
 		if ($this->pid !== null)
 			$this->model->setDefault($this->pid);
@@ -25,17 +77,26 @@ class ProgramDataController extends BaseController {
 
 	protected function initFromRequest() {
 
-		// check for program id else we get actual program
+		// check for program id
 		$this->pid = Request::Attr('program', null);
 
-		( !Validate::IsInteger($this->pid)
-			|| !$this->model->exists($this->pid)
-		) && $this->pid = null;
+		!Validate::IsInteger($this->pid)
+			&& $this->pid = null;
 
 		$this->day = Request::Attr('day', null);
 
 		($this->day === '' ||  !Validate::IsDayOfWeek($this->day))
 			&& $this->day = date('N');
+
+		$this->name = Request::Attr('name', null);
+		$this->descr = Request::Attr('description', null);
+		$this->sensor = Request::Attr('sensor', null);
+
+		$temps = Request::Attr('temperature', null);
+
+		if (is_array($temps))
+			foreach ($temps as $t)
+				$this->temps[] = $t;
 	}
 
 }

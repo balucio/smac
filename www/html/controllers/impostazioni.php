77b64af86c7ed16e298571c9b8803597ee8d29da
Saul Bertuccio <bsaul@inwind.it>
2016-03-22 03:59:34 +0100

    Aggiunta funzione per la configurazione dei sensori

--- www/html/controllers/impostazioni.php
+++ www/html/controllers/impostazioni.php
@@ -2,15 +2,22 @@
 
 class ImpostazioniController extends BaseController {
 
-    public function __construct($model) {
+	public function __construct($model) {
 
-        parent::__construct($model, false);
+		parent::__construct($model, false);
 
-        $this->setDefaultAction('view');
-    }
+		$this->setDefaultAction('view');
+	}
 
-    public function programmi() {
+	public function programmi() {
 
-        $this->model->setPid(ProgramModel::CURRENT_PROGRAM, ProgramDataModel::DAY_ALL, ProgramListModel::NONE);
-    }
+		$this->model->setPid(ProgramModel::CURRENT_PROGRAM, ProgramDataModel::DAY_ALL, ProgramListModel::NONE);
+	}
+
+	public function sensori() {
+
+			// Get the first sensor
+		$this->model->setSid( 1, SensorListModel::ALL );
+	}
+
 }

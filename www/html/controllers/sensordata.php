87c6b6a7ac188a33482f747df4ef82b3533140ae
Saul Bertuccio <bsaul@inwind.it>
2016-03-28 22:24:13 +0200

    Aggiunte operazioni di modifica ed eliminazione sui sensori

--- www/html/controllers/sensordata.php
+++ www/html/controllers/sensordata.php
@@ -9,23 +9,65 @@ class SensorDataController extends BaseController {
 	public function __construct($model, $init = true) {
 
 		parent::__construct($model, $init);
-		$this->setDefaultAction('getData');
+		$this->setDefaultAction('getMeasure');
 	}
 
 	public function getMeasure() {
 
+		$this->sid = (int)($this->model->exists($this->sid) ? $this->sid : 0);
 		$this->model->collectEnviromentalData($this->sid);
 	}
 
 	public function getData() {
+		$this->sid = (int)($this->model->exists($this->sid) ? $this->sid : 0);
 
 		$this->model->collectData($this->sid);
 	}
 
-	protected function initFromRequest() {
+	public function delete() {
+
+ 		if (!Validate::IsPositiveInt( $this->sid ))
+			return;
+
+		$this->model->delete($this->sid);
+
+ 	}
+
+	public function createOrUpdate() {
 
 		$sid = Request::Attr('sensor', null);
 
-		$this->sid = (int)($this->model->exists($sid) ? $sid : 0);
+			// Verifico che il sid originario sia comunque corretto
+		if ($sid != $this->sid)
+			return;
+
+		$driver = Request::Attr('driver', null);
+
+		if (!Validate::IsPositiveInt( $driver ))
+			return;
+
+		$p = [
+			'sid' => $sid,
+			'driver' => $driver,
+			'inaverage' => Request::Attr('inaverage', null) == 't',
+			'enabled' => Request::Attr('enabled', null) == 't'
+		];
+
+			// Verifico presenza di nome programma, descrizione e temperature
+		foreach (['name', 'description', 'parameters'] as $k) {
+			$v = Request::Attr($k, null);
+			if (empty($v))
+				return;
+			$p[$k] = $v;
+		}
+
+		$this->model->updateSensor($p);
+	}
+
+
+	protected function initFromRequest() {
+
+		$this->sid = Request::Attr('sensor', null);
+
 	}
 }

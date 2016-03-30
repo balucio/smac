461255bb0fe2bb9d5e51381ca596cf1ffd7be65c
Saul Bertuccio <bsaul@inwind.it>
2016-03-30 22:37:49 +0200

    Implementata la funzione di salvataggio delle impostazioni generali

--- www/html/controllers/impostazioni.php
+++ www/html/controllers/impostazioni.php
@@ -22,6 +22,35 @@ class ImpostazioniController extends BaseController {
 
 	public function generali() {
 
+		$action = Request::Attr('action', null);
+
+		if ($action != null) {
+			$this->saveData();
+			// Request::Redirect($_SERVER['PHP_SELF']);
+		}
+	}
+
+	protected function saveData() {
+
+		$sa = Request::Attr('sensore_antigelo', null);
+
+		if (Validate::IsInteger($sa) && ( $sa == 0 || $this->model->exists($sa) ) )
+			$this->model->antifreezeSensor = $sa;
+
+		$ta = Request::Attr('temperatura_antigelo', null);
+
+		if (Validate::IsFloatInRange($ta, Validate::MIN_TEMP, Validate::MAX_TEMP ))
+			$this->model->antifreezeTemp = $ta;
+
+		$sm = Request::Attr('sensore_manuale', null);
+
+		if (Validate::IsInteger($sm) && ( $sm == 0 || $this->model->exists($sm)) )
+			$this->model->manualSensor = $sm;
+
+		$tm = Request::Attr('temperatura_manuale', null);
+
+		if (Validate::IsFloatInRange($tm, Validate::MIN_TEMP, Validate::MAX_TEMP ))
+			$this->model->manualTemp = $tm;
 	}
 
 }

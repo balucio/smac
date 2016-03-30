1ec79cc837b3835aeded676d8a31285cb805642e
Saul Bertuccio <bsaul@inwind.it>
2016-03-30 22:40:48 +0200

    Implementate funzioni per implementare il salvataggio e la lettura delle impostazioni. Le altre funzioni sono state implementate come wrapper dei metodi delle funzioni dell'oggetto modello sensordata

--- www/html/models/mainsettings.php
+++ www/html/models/mainsettings.php
@@ -3,21 +3,77 @@
 class MainSettingsModel {
 
 	private
-		$slist = null
+
+		$db = null
 	;
 
 	public function __construct() {
 
-		$this->slist = new SensorListModel();
+		$this->db = Db::get();
+
 	}
 
-	public function _isset($d) {
+	public function exists($sid) {
 
-		return true;
+		$sd = new SensorDataModel();
+		return $sd->exists($sid);
+	}
+
+	public function __set($d, $v) {
+
+		$setting_name = null;
+
+		switch ($d) {
+
+			case 'manualSensor' : $setting_name = Db::CURR_MANUAL_SENSOR;
+				break;
+
+			case 'antifreezeSensor' : $setting_name = Db::CURR_ANTIFREEZE_SENSOR;
+				break;
+
+			case 'manualTemp' : $setting_name = Db::CURR_MANUAL_TEMP;
+				break;
+
+			case 'antifreezeTemp' : $setting_name = Db::CURR_ANTIFREEZE_TEMP;
+		}
+
+		if ($setting_name != null)
+			$this->db->saveSetting($setting_name, $v);
 	}
 
 	public function __get($d) {
 
-		return '';
+		switch ($d) {
+			case 'list' :
+				return $this->getList();
+
+			case 'manualSensor' :
+				return $this->db->readSetting(Db::CURR_MANUAL_SENSOR, 0);
+
+			case 'antifreezeSensor' :
+				return $this->db->readSetting(Db::CURR_ANTIFREEZE_SENSOR, 0);
+
+			case 'manualTemp' :
+				return $this->db->readSetting(Db::CURR_MANUAL_TEMP, 20);
+
+			case 'antifreezeTemp' :
+				return $this->db->readSetting(Db::CURR_ANTIFREEZE_TEMP, 5);
+
+			default:
+				return null;
+		}
 	}
+
+	private function getList() {
+
+		$slist = new SensorListModel();
+		$slist->enumerate(SensorListModel::ENABLED, -1);
+		return $slist->get(true);
+	}
+
+
+	public function _isset($d) {
+		return in_array($d, ['list', 'manualSensor', 'antifreezeSensor', 'manualTemp', 'antifreezeTemp']);
+	}
+
 }

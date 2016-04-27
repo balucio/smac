78b77070bbd942e8bb10d877cca6b9bdaf5a742e
Saul Bertuccio <bsaul@tiscali.it>
2016-04-27 08:11:53 +0200

    Modifica temporanea per output in standard error

--- bin/daemon.py
+++ bin/daemon.py
@@ -18,7 +18,7 @@ class Daemon:
     Usage: subclass the Daemon class and override the run() method
     """
     def __init__(self, pidfile, stdin='/dev/null',
-                 stdout='/dev/null', stderr='/dev/null',
+                 stdout='/dev/stdout', stderr='/dev/stderr',
                  logfile='/dev/null'):
         self.stdin = stdin
         self.stdout = stdout

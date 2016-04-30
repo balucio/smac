0b6269e5026dd4e320d8aa2f61c42d47a4dc2dfc
Saul Bertuccio <bsaul@tiscali.it>
2016-04-30 22:56:02 +0200

    Modifiche minori all'identazione del codice

--- bin/daemon.py
+++ bin/daemon.py
@@ -11,15 +11,15 @@ from smac_utils import setup_logger
 from logging import getLogger
 
 
-class Daemon:
+class Daemon(object):
     """
     A generic daemon class.
 
     Usage: subclass the Daemon class and override the run() method
     """
-    def __init__(self, pidfile, stdin='/dev/null',
-                 stdout='/dev/stdout', stderr='/dev/stderr',
-                 logfile='/dev/null'):
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null',
+                 stderr='/dev/null', logfile='/dev/null'):
+
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
@@ -159,7 +159,7 @@ class Daemon:
                 if os.path.exists(self.pidfile):
                     self.delpid()
             else:
-                print str(err)
+                print(str(err))
                 sys.exit(1)
 
     def reload(self):

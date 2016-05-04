7317cf3097b883030e1c212e9a10cc7dfd0dddb7
Saul Bertuccio <bsaul@tiscali.it>
2016-05-04 07:31:12 +0200

    La vista prova a gestire sia il comando di stato che quelli di azione on off

--- www/html/views/switcher.php
+++ www/html/views/switcher.php
@@ -7,19 +7,33 @@ class SwitcherView extends BaseView {
 		if (is_string($this->model->result))
 
 			switch ($this->model->result) {
-				case 'ON': $classes = 'fa fa-fire status on';
+				case 'ON':
+					$classes = 'fa fa-fire status on';
+					$title = 'title-status-on';
 					break;
-				case 'OFF': $classes = 'fa fa-fire status off';
+				case 'OFF':
+					$classes = 'fa fa-fire status off';
+					$title = 'title-status-off';
 					break;
-				default: $classes = 'fa fa-exclamation-circle status undefined';
+				default:
+					$classes = 'fa fa-exclamation-circle status undefined';
+					$title = 'title-status-unknow';
 			}
 
-		else if (is_bool($this->model->result))
+		else if (is_bool($this->model->result)) {
 			$classes = $this->model->result ? 'success' : 'danger';
-		else
+			$title = ''; // TODO
+		}
+		else {
 			$classes = 'fa fa-exclamation-triangle status undefined';
+			$title = 'title-op-error'; // TODO
+		}
 
-		$result = (object)[ 'result' => $this->model->result, 'classes' => $classes ];
+		$result = (object)[
+			'result' => $this->model->result,
+			'classes' => $classes,
+			'title' => $title
+		];
 		return json_encode($result, JSON_NUMERIC_CHECK);
 	}
 }

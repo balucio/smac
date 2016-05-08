ac4f818a004595321de562d122fb632aec53d5d8
Saul Bertuccio <bsaul@tiscali.it>
2016-05-08 08:06:53 +0200

    Eliminate le funzioni relative al controllo del relè tramite il programma esterno in python che usa le named pipe per comunicare con lo switcher. Le stesse funzioni saranno implementate come eventi sul database. La funzione status utilizza adesso la tabella ultima_commutazione per rilevare lo stato del relè. Questa tabella è pilotata tramite trigger dalla tabella storico_commutazioni, aggiornata a sua volta dall'actuator

--- www/html/models/switcher.php
+++ www/html/models/switcher.php
@@ -2,10 +2,6 @@
 
 class SwitcherModel {
 
-	const
-		TIMEOUT_BIN = "/usr/bin/timeout",
-		SWITCHER_BIN = "switcher_comunicator.py"
-	;
 	public
 		$result = null
 	;
@@ -16,46 +12,21 @@ class SwitcherModel {
 
 	public function state() {
 
-		if ($this->send_swicher_command('state', $res))
-			$this->result = $res;
+		$this->result = Db::get()->getFirstRow(
+			'SELECT * FROM ultima_commutazione WHERE NOW() - data_ora <= :max_min::interval',
+			[ ':max_min' => '10 minutes' ]
+		);
 	}
 
 	public function on() {
 
-		if ($this->send_swicher_command('on', $res))
-			$this->result = $res == 'True' ? true : false;
 	}
 
 	public function off() {
 
-		if ($this->send_swicher_command('off', $res))
-			$this->result = $res == 'True' ? true : false;
 	}
 
 	public function reload() {
 
-		if ($this->send_swicher_command('reload', $res))
-			$this->result = $res == 'True' ? true : false;
-	}
-
-	private function send_swicher_command($params, &$out, $timeout=5) {
-
-		is_array($params)
-			|| $params = array($params);
-
-		$esc_exe = escapeshellcmd(
-			APP_DIR . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . self::SWITCHER_BIN
-		);
-
-		foreach ($params as &$p)
-			$p = escapeshellarg($p);
-
-
-		$command  = escapeshellcmd(self::TIMEOUT_BIN) . ' ' . escapeshellarg($timeout)
-			. ' ' . $esc_exe  . ' ' . implode(' ', $params);
-
-		$out = exec( $command, $full_out, $ret );
-
-		return $ret == 0;
 	}
 }

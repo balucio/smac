4922b478691fcae7b3c8332fbe9ab8d799b4a8f6
Saul Bertuccio <saul.bertuccio@netserv.it>
2016-05-14 22:23:59 +0200

    Modificato il controller per essere poter gestire sia le richieste di grafici dalla pagina di stato, sia per quelle dalla pagina statistiche. Rimossa l'obbligatorietà dell'id sensore, sarà il modello corretto a non generare dati se mancano i requisiti. Aggiunta variabile che rappresenta il numero di punti massimo da ritornare per i grafici. Aggiunto aggiornamento periodico delle statistiche giornaliere se implementato nel modello

--- www/html/controllers/sensorstats.php
+++ www/html/controllers/sensorstats.php
@@ -4,16 +4,30 @@ class SensorStatsController extends BaseController {
 
 	const
 
+		// Soglia aggiornamento statistiche odierne
+		THR_STATS_UPDATE = 3600,
 		// Secondi in un ora
 		DEF_INTERVAL = 3600,
 		// Tre mesi
-		MAX_INTERVAL = 8035200
+		MAX_INTERVAL = 8035200,
+		// Numero massimo di punti da processare
+		MAX_POINT_NUM = 300
 	;
 
 	public function __construct($model, $init = true) {
 
 		parent::__construct($model, $init);
 		$this->setDefaultAction('temperatura');
+
+		$lu = isset($_SESSION['today_stats_last_update'])
+			? $_SESSION['today_stats_last_update'] : 0;
+
+		if (time() - $lu > self::THR_STATS_UPDATE
+			&& method_exists($this->model, 'update_today_stats')
+		) {
+				$this->model->update_today_stats();
+				$_SESSION['today_stats_last_update'] = time();
+		}
 	}
 
 	public function temperatura() {
@@ -30,10 +44,20 @@ class SensorStatsController extends BaseController {
 
 		$sid = Request::Attr('sensor', null);
 
-		if (!Validate::IsPositiveInt($sid) && $sid != '0')
-			return;
+		if (!is_null($sid)
+			&& ($sid == '0' || Validate::IsPositiveInt($sid))
+		) {
+			$this->model->setSensorId($sid);
+		}
 
-		$this->model->setSensorId($sid);
+		$points = Request::Attr('points_number', null);
+
+		if (!is_null($points) 
+			&& Validate::IsPositiveInt($points)
+			&& $points <= self::MAX_POINT_NUM
+
+		)
+			$this->model->setNumberOfPoint($points);
 
 		$int = Request::Attr('interval', null);
 		$int = Validate::IsPositiveInt($int) ? $int : self::DEF_INTERVAL;

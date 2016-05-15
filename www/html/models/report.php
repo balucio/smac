176094336a092134f43e00f0899ec056ed8e43f5
Saul Bertuccio <bsaul@tiscali.it>
2016-05-15 22:15:40 +0200

    Eliminata funzione creazione statistiche giornaliere sui sensori, spostata nel modello Statistiche che viene usato nella pagina principale delle statistiche

--- www/html/models/report.php
+++ www/html/models/report.php
@@ -70,15 +70,4 @@ class ReportModel {
 		$stmt->execute();
 		return $stmt->fetchAll(PDO::FETCH_NUM);
 	}
-
-	/* Aggiorna i dati della giornata odierna */
-	public function update_today_stats() {
-		$query = "SELECT aggiorna_dati_giornalieri(id_sensore)" 
-				 ." FROM sensori" 
-				." WHERE abilitato = true";
-		// Eseguo e scarto i dati
-		Db::get()->query($query);
-	}
-
-
 }

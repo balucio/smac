a6b5724594729d2e3c7a0d1d4c9077245e0d34b5
Saul Bertuccio <saul.bertuccio@netserv.it>
2016-05-27 10:59:56 +0200

    Inserito wrapper per il decoratore degli intervalli

--- www/html/classes/templateextension.class.php
+++ www/html/classes/templateextension.class.php
@@ -12,6 +12,7 @@ class TemplateExtension extends Twig_Extension {
 			new Twig_SimpleFilter('Temperature', array($dec, 'decorateTemperature')),
 			new Twig_SimpleFilter('DateTime', array($dec, 'decorateDateTime')),
 			new Twig_SimpleFilter('ShortDay', array($dec, 'decorateShortDay')),
+			new Twig_SimpleFilter('Interval', array($dec, 'decorateInterval')),
 			new Twig_SimpleFilter('Umidity', array($dec, 'decorateUmidity')),
 			new Twig_SimpleFilter('Time', array($dec, 'decorateTime')),
 			new Twig_SimpleFilter('Day', array($dec, 'decorateDay')),

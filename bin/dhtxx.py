24147ddcc6b9ba34063d2eafce2952a9d94cf41b
Saul Bertuccio <bsaul@tiscali.it>
2016-07-31 21:16:00 +0200

    Il driver scarta la lettura e ne esegue una nuova se il parametro umidità restituito dal sensore non ha senso (inferiore a zero o maggiore di 100)

--- bin/dhtxx.py
+++ bin/dhtxx.py
@@ -48,10 +48,25 @@ parser.add_argument(
 args = parser.parse_args()
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(args.sensor, args.pin,
-                                                args.retries,
-                                                args.delay_seconds)
+
+c_retry = 0
+humidity = None
+temperature = None
+
+while c_retry < args.retries: 
+    humidity, temperature = Adafruit_DHT.read_retry(args.sensor, args.pin,
+                                                    args.retries,
+                                                    args.delay_seconds)
+    c_retry += 1
+
+    if humidity is None:
+        break
+
+    if humidity > 0 and humidity < 100:
+        break
+
+    humidity = None
+    temperature = None
 
 if humidity is not None and temperature is not None:
     print "{:f} {:f}".format(temperature, humidity)
